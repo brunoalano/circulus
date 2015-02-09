9b7e2ed96854dc7f74b8538ffbab7b380d2bb910
Bruno Alano <bruno@appzlab.com>
2015-02-09 15:48:21 -0200

    Base structure
    Added a lot of details about the structure of project, like GNU GPLv3 license and some of Knupp methodology

---
+++ README.md
@@ -0,0 +1,4 @@
+Circulus: File Sync <2
+============
+
+Circulus is an file synchorinization service for OSX that uses Amazon AWS S3 and Glacier for cheaper file storage. It's a replacement to Dropbox.
---
+++ setup.py
@@ -0,0 +1,18 @@
+setup(
+  # Project
+  name = 'circulus',
+  version = circulus.__version__,
+  url = 'http://github.com/brunoalano/circulus/',
+  license = 'Apache Software License',
+
+  # Author
+  author = 'Bruno Alano Medina',
+  author_email = 'bruno@appzlab.com',
+
+  # Details
+  packages = find_packages(),
+  install_requires=[],
+  description = '',
+  long_description = '''Long description''',
+  platforms = 'any'
+)
